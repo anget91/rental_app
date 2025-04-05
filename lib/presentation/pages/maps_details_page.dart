273d4b9b1dabfc0549605bd22fd25b42c8bddf8e
BrayanEFJ <atolozafranco13@gmail.com>
2025-04-04 20:31:22 -0500

    feat: enhance maps details page with car details card widget

--- lib/presentation/pages/maps_details_page.dart
+++ lib/presentation/pages/maps_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:rental_app/presentation/widgets/car_details_card.dart';
 
 class MapsDetailsPage extends StatelessWidget {
   const MapsDetailsPage({super.key});
@@ -8,20 +9,33 @@ class MapsDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('MapsDetailsPage'),
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(51, -0.09),
-          initialZoom: 13.0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
+      ),
+      body: Stack(
         children: [
-          TileLayer(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: LatLng(51, -0.09),
+              initialZoom: 13.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: ['a', 'b', 'c'],
+              ),
+            ],
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: carDetailsCard(),)
         ],
       ),
     );
---
+++ lib/presentation/widgets/car_details_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+Widget carDetailsCard() {
+  return SizedBox(
+    height: 350,
+    child: Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            boxShadow: [
+              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+            ],
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Text(
+                "Car.model",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(Icons.directions_car, color: Colors.white, size: 16),
+                  SizedBox(width: 5),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
